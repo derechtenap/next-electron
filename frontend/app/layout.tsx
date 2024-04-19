4231127a916424fbb5ba2164561eb01a2e730e21
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-04-19 15:04:56 +0200

    Add mantine component lib

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,4 +1,12 @@
+import '@mantine/core/styles.css'
 import './globals.css'
+
+import {
+	ColorSchemeScript,
+	type MantineColorScheme,
+	MantineProvider,
+	createTheme,
+} from '@mantine/core'
 
 type RootLayoutProps = {
 	children: React.ReactNode
@@ -11,9 +19,32 @@ export const metadata = {
 }
 
 export default function RootLayout({ children }: RootLayoutProps) {
+	/*
+	 * By setting `ColorSchemeScript` and `MantineProvider` to `auto`, the app
+	 * will automatically adapt to the user's OS color scheme. However, users
+	 * can override this behavior by setting the scheme to either `light` or
+	 * `dark` in the app settings.
+	 */
+	const defaultColorScheme: MantineColorScheme = 'auto'
+
+	const appTheme = createTheme({
+		// Add theme overrides here.
+		primaryColor: 'green',
+	})
+
 	return (
 		<html lang="en">
-			<body>{children}</body>
+			<head>
+				<ColorSchemeScript defaultColorScheme={defaultColorScheme} />
+			</head>
+			<body>
+				<MantineProvider
+					defaultColorScheme={defaultColorScheme}
+					theme={appTheme}
+				>
+					{children}
+				</MantineProvider>
+			</body>
 		</html>
 	)
 }
