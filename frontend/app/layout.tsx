330bdaa77b1c225f1c38a372aa34b42b6517d41a
Tim Deres <50623315+derechtenap@users.noreply.github.com>
2024-04-19 14:15:28 +0200

    Removed app folder boiler code

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -1,16 +1,16 @@
 import './globals.css'
 
+type RootLayoutProps = {
+	children: React.ReactNode
+}
+
 export const metadata = {
 	title: 'NextJS + Electron Boilerplate',
 	description:
 		'A neat boilerplate for building Electron apps, with NextJS at the frontend and pre-configured with a bunch of handy development tools.',
 }
 
-export default function RootLayout({
-	children,
-}: {
-	children: React.ReactNode
-}) {
+export default function RootLayout({ children }: RootLayoutProps) {
 	return (
 		<html lang="en">
 			<body>{children}</body>
